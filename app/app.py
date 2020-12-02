a05baef11824031b6af22974d0e8b5efa33ef3cc
Ludovic Mantovani <ludovic.mantov@gmail.com>
2020-12-02 23:42:33 +0100

    [IMP] 2 Create add function and use it

--- app/app.py
+++ app/app.py
@@ -2,6 +2,14 @@
 
 def main():
     print("Hello world !")
+    n1 = 1
+    n2 = 2
+    n3 = add(n1, n2)
+    result = "{} + {} = {}".format(n1, n2, n3)
+    print(result)
+
+def add(n1, n2):
+    return n1 + n2
 
 if __name__ == '__main__':
     main()
